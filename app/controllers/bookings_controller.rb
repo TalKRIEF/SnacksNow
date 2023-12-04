5e45f6f9845529ed4480db2996459a0999747f11
heba hakimi <heba.hakimi@hotmail.fr>
2023-12-04 17:05:59 +0100

    create de bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,30 @@
 class BookingsController < ApplicationController
 
   def create
-    @booking = Booking.new(booking_params)
-    # @booking.snack = @snack
-    @booking.save
+    @booking = Booking.new
+    @snack = Snack.find(params[:snack_id])
+    @booking.snack = @snack
+    @booking.user = current_user
+    @booking.booked = false
 
-    redirect_to snack_path(@snack)
+    if @booking.save
+      redirect_to snack_path(@snack)
+    else
+      render :snack_path
+    end
   end
 
-  def accept
+  # def accept
+  #   if @booking.accepted!
+  #     redirect_to @booking, notice: 'Offer accepted'
 
+  #   end
 
-  end
+  # end
 
-  private
+  # private
 
-  def booking_params
-    params.require(:booking).permit(:user_id, :snack_id)
-  end
+  # def booking_params
+  #   params.require(:booking).permit(:user_id, :snack_id)
+  # end
 end
