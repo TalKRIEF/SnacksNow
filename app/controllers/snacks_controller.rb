14d88a8bff4af09fb7bcc91178e542c332e6af51
Benjamin Galland <galland.benjamin98@gmail.com>
2023-12-04 16:33:14 +0100

    snacks new & create and unfinished edit

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,5 +1,5 @@
 class SnacksController < ApplicationController
-before_action :set_snack, only: [:show]
+before_action :set_snack, only: [:show, :edit]
 
   def index
     @snacks = Snack.all;
@@ -8,9 +8,32 @@ before_action :set_snack, only: [:show]
   def show
   end
 
+  def new
+    @snack = Snack.new
+  end
+
+  def create
+    @snack = Snack.new(snack_params)
+    # user_id = current_user.id
+    # @snack.user_id = user_id
+    @snack.user = current_user
+    if @snack.save
+      redirect_to snacks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
   private
 
   def set_snack
     @snack = Snack.find(params[:id])
+  end
+
+  def snack_params
+    params.require(:snack).permit(:name, :description, :price)
   end
 end
