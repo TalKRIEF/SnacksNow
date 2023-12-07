9332e618265549ef9798bfb8081ad63a61e08cd4
Benjamin Galland <galland.benjamin98@gmail.com>
2023-12-07 14:54:26 +0100

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_090848) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_134430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_090848) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "snack_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["snack_id"], name: "index_reviews_on_snack_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "snacks", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -83,5 +94,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_090848) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "snacks"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "snacks"
+  add_foreign_key "reviews", "users"
   add_foreign_key "snacks", "users"
 end
